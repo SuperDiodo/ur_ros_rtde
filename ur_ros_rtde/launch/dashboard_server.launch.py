7d65fe7a8c00a3dc5735c4db5a12bbce4c5eedae
SuperDiodo <alessio.saccuti@gmail.com>
2024-07-31 21:10:59 +0200

    add general dashboard server launch file

---
+++ ur_ros_rtde/launch/dashboard_server.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    ######################
+    #### CHANGE HERE #####
+
+    dashboard_server_params = {
+        "robot_ip": "", # robot ip
+    }
+
+    ######################
+    #### DO NOT TOUCH ####
+
+    nodes = []
+
+    nodes.append(
+        Node(
+            package="ur_ros_rtde",
+            executable="dashboard_server",
+            namespace="ur_ros_rtde",
+            output="screen",
+            parameters=[
+                dashboard_server_params,
+            ],
+        )
+    )
+
+    return LaunchDescription(nodes)
